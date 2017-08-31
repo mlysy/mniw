18fff85940e7ba13b66c706f2a60c1daf2dca8b9
mlysy <mlysy@users.noreply.github.com>
2017-08-31 10:30:08 -0400

    added Hospital dataset of Everson-Morris00

---
+++ R/hospitals.R
@@ -0,0 +1,13 @@
+#' Hospital profiling data.
+#'
+#' Information on patient-reported problem rates for 27 teaching hosptials and private academic health centers in the United States.
+#'
+#' @format A data frame with 27 rows (one for each hospital) and 4 variables:
+#' \describe{
+#'   \item{\code{NSrg}}{Non-surgery related problem rate (\%).}
+#'   \item{\code{Srg}}{Surgery related problem rate (\%).}
+#'   \item{\code{Severity}}{Average health index for surveyed patients.}
+#'   \item{\code{Size}}{Number of patients surveyed.}
+#' }
+#' @references Everson & Morris, 2000; original data in Cleary et al, 1991.
+"Hospitals"
