d84ff94c616fa1b9af3eff757362b4faba40868b
mlysy <mlysy@users.noreply.github.com>
2019-04-17 20:40:12 -0400

    added visual tests for RxNormLM

--- examples/Hierarchical.R
+++ examples/Hierarchical.R
@@ -1,27 +1,120 @@
-# Input Specifications
-nsamples <- 10
-N <- 20
-p <- 1
-q <- 2
-# prior specification
-Lambda0 <- rMNorm(1, Lambda = matrix(0, p, q))
-Omega0 <- crossprod(rMNorm(1, Lambda = matrix(0, p, p)))
-Psi0 <- crossprod(rMNorm(1, Lambda = matrix(0, q, q)))
-nu0 <- rexp(1) + (q+1)
-# covariate specification
-X <- rMNorm(1, matrix(0, N, p))
-V <- array(rwish(N, diag(q), q+1),dim=c(q,q,N))
+# problem dimensions
+n <- sample(10:20,1) # number of observations
+p <- sample(1:4,1) # number of covariates
+q <- sample(1:4,1) # number of responses
+
+# hyperparameters
+Lambda <- rMNorm(1, Lambda = matrix(0, p, q))
+Omega <- crossprod(rMNorm(1, Lambda = matrix(0, p, p)))
+Psi <- crossprod(rMNorm(1, Lambda = matrix(0, q, q)))
+nu <- rexp(1) + (q+1)
+prior <- list(Lambda = Lambda, Omega = Omega, Psi = Psi, nu = nu)
+
+# random-effects parameters
+BSig <- rmniw(1, Lambda = Lambda, Omega = Omega, Psi = Psi, nu = nu)
+Beta <- BSig$X
+Sigma <- BSig$V
+
+# design matrix
+X <- rMNorm(1, matrix(0, n, p))
+
+# random-effects themselves
+Mu <- rmNorm(n, X %*% Beta, Sigma)
 
 # generate response data
-Sigma00 <- rwish(1, Psi0, nu0)
-Beta00 <- rMNorm(1, Lambda0, Omega0, Sigma00)
-Mu00 <- rmNorm(N, X %*% Beta00, Sigma00) # random effects
-Y00 <- rmNorm(N, Mu00, V) # Data
+V <- rwish(n, Psi = diag(q), nu = q+1) # error variances
+Y <- rmNorm(n, mu = Mu, Sigma = V) # responses
 
-# Gibbs sampling to get posterior
-prior_list <- list(Psi=Psi0, Lambda=Lambda0, Omega=solve(Omega0), nu=nu0) # Prior
-init_list <- list(Beta=Beta00,Sigma=Sigma00,Mu=Mu00) # Initialize MCMC
-r_fit <- RxNormLM(nsamples, Y=Y00, V=V, X=X,
-                  prior = prior_list,
-                  init = init_list,
-                  burn = ceiling(nsamples/2))
+# visual checks for each component of Gibbs sampler
+
+# sample from p(Mu | Beta, Sigma, Y)
+nsamples <- 1e5
+out <- RxNormLM(nsamples,
+                Y = Y, V = V, X = X,
+                prior = prior,
+                init = list(Beta = Beta, Sigma = Sigma, Mu = Mu),
+                burn = floor(nsamples/10),
+                updateHyp = FALSE,
+                storeHyp = FALSE,
+                updateRX = TRUE,
+                storeRX = TRUE)
+
+# conditional distribution is RxNorm:
+iObs <- sample(n, 1) # pick an observation at random
+# calculate the RxNorm parameters
+G <- Sigma %*% solve(V[,,iObs] + Sigma)
+xB <- c(X[iObs,,drop=FALSE] %*% Beta)
+muRx <- G %*% (Y[iObs,] - xB) + xB
+SigmaRx <- G %*% V[,,iObs]
+
+# a' * mu_i is univariate normal with known mean and variance:
+a <- rnorm(q) # arbitrary vector
+amui <- crossprod(a, out$Mu[iObs,,]) # a' * mu_i
+
+hist(amui, breaks = 100, freq = FALSE,
+     xlab = "", main = expression("Histogram of "*a^T*mu[i]))
+curve(dnorm(x, mean = sum(a * muRx),
+            sd = sqrt(crossprod(a, SigmaRx %*% a)[1])),
+      add = TRUE, col = "red")
+legend("topright",
+       legend = c("Observed", "Expected"),
+       lwd = c(NA, 2), pch = c(22, NA), seg.len = 1.5,
+       col = c("black", "red"), bg = c("white", NA))
+
+
+
+# sample from p(Beta, Sigma | Mu, Y)
+nsamples <- 1e5
+out <- RxNormLM(nsamples,
+                Y = Y, V = V, X = X,
+                prior = prior,
+                init = list(Beta = Beta, Sigma = Sigma, Mu = Mu),
+                burn = floor(nsamples/10),
+                updateHyp = TRUE,
+                storeHyp = TRUE,
+                updateRX = FALSE,
+                storeRX = FALSE)
+
+
+# conditional distribution is MNIW:
+# calculate the MNIW parameters
+OmegaHat <- crossprod(X) + Omega
+LambdaHat <- solve(OmegaHat, crossprod(X, Mu) + Omega %*% Lambda)
+PsiHat <- Psi + crossprod(Mu) + crossprod(Lambda, Omega %*% Lambda)
+PsiHat <- PsiHat - crossprod(LambdaHat, OmegaHat %*% LambdaHat)
+nuHat <- nu + n
+
+# a' Sigma^{-1} a is chi^2 with known parameters:
+a <- rnorm(q)
+aSiga <- drop(crossprodV(a, V = out$Sigma, inverse = TRUE))
+sigX <- crossprod(a, solve(PsiHat, a))[1]
+hist(aSiga, breaks = 100, freq = FALSE,
+     xlab = "", main = expression("Histogram of "*a^T*Sigma^{-1}*a))
+curve(dchisq(x/sigX, df = nuHat)/sigX, add = TRUE, col = "red")
+legend("topright",
+       legend = c("Observed", "Expected"),
+       lwd = c(NA, 2), pch = c(22, NA), seg.len = 1.5,
+       col = c("black", "red"), bg = c("white", NA))
+
+# a' Beta b is student-t with known parameters:
+a <- rnorm(p)
+b <- rnorm(q)
+# vectorized calculations
+aBetab <- crossprodV(X = aperm(out$Beta, c(2,1,3)),
+                     Y = b, V = diag(q)) # Beta b
+aBetab <- drop(crossprodV(X = a, Y = aBetab, V = diag(p))) # a' Beta b
+# student-t parameters
+muT <- crossprod(a, LambdaHat %*% b)[1]
+nuT <- nuHat-q+1
+sigmaT <- crossprodV(a, V = OmegaHat, inverse = TRUE)[1]
+sigmaT <- sigmaT * crossprodV(b, V = PsiHat)[1]
+sigmaT <- sqrt(sigmaT / nuT)
+
+hist(aBetab, breaks = 100, freq = FALSE,
+     xlab = "", main = expression("Histogram of "*a^T*Beta*a))
+curve(dt((x-muT)/sigmaT, df = nuT)/sigmaT, add = TRUE, col = "red")
+legend("topright",
+       legend = c("Observed", "Expected"),
+       lwd = c(NA, 2), pch = c(22, NA), seg.len = 1.5,
+       col = c("black", "red"), bg = c("white", NA))
+
